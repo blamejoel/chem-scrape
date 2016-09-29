5ae183f056707b568660071592b3df079ae06ba8
Joel Gomez <joel.gomez@email.ucr.edu>
2016-09-29 01:51:58 -0700

    making progress

--- README.md
+++ README.md
@@ -1,45 +1,8 @@
-## UCR Term Dates
-This is a very basic API that scrapes the UCR Academic Calendar "print preview"
-page [http://registrar.ucr.edu/registrar/academic-calendar/print-academic-calendar.html]
-(http://registrar.ucr.edu/registrar/academic-calendar/print-academic-calendar.html) 
-for the First and Last day of instruction for a given quarter term and year.
-
-The first and last day of instruction are returned in json format along with 
-the original term and year requested.
+## chem-scrape
 
 ### Using the API
-The API call format is https://ucr-term-dates.appspot.com/[quarter]?year=[year]
-
-The response will be a json result in the format
-```json
-{
-    "end": "YYYY-MM-DD", 
-    "start": "YYYY-MM-DD", 
-    "term": "[quarter]", 
-    "year": "[year]"
-}
-```
-
-For example, a `GET` request to the URL 
-https://ucr-term-dates.appspot.com/fall?year=2016 
-will give a json response
-```json
-{
-    "end": "2016-12-02", 
-    "start": "2016-09-22", 
-    "term": "FALL", 
-    "year": "2016"
-}
-```
-
-An invalid request or a request for a term no listed on the UCR Academic 
-Calendar page will return `-1` for both `end` and `start`.
 
 ### Known Issues
-This API will only scrape the UCR Academic Calendar print preview page. At the 
-time that this was developed, Summer Sessions are NOT listed on that page and 
-will therefore result in an erroneous response of `-1` for the first and last 
-days of instruction.
 
 ### Feedback
 Star this repo if you found it useful. Use the github issue tracker to give
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests,datetime,re,sys
+import requests,datetime,re,sys,json
 from datetime import timedelta
 from flask import Flask, request, Response, send_from_directory, url_for, \
         jsonify, make_response, current_app
@@ -11,8 +11,15 @@ app = Flask(__name__)
 # app = Flask(__name__, static_url_path='')
 
 
-ucr_cal = 'http://registrar.ucr.edu/registrar/academic-calendar/' + \
-        'print-academic-calendar.html'
+base_pub = 'https://pubchem.ncbi.nlm.nih.gov/compound/'
+base_pub2 = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/'
+base_ncbi = 'https://www.ncbi.nlm.nih.gov/'
+c_search_path = 'pccompound?term='
+headers = {'user-agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; \
+rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'}
+
+
+compound = 'Nitropyrene'
 
 
 # Cross domain request decorator
@@ -61,32 +68,154 @@ def crossdomain(origin=None, methods=None, headers=None,
 
 
 # Crappy API
-@app.route('/<req_term>')
+@app.route('/<req_compound>')
 @crossdomain(origin='*')
-def json_output(req_term):
-    req_year = request.args.get('year')
-    if req_year > 2015:
-        search_year = req_year
+def json_output(req_compound):
+    req_num = int(request.args.get('num', 0, type=int))
+    cid = getPubChemID(req_compound, req_num)
+    url2get = base_pub2 + cid + '/JSON/'
+    print url2get
+    r = requests.get(url2get, headers=headers)
+    html = r.text
+    soup = BeautifulSoup(html, "html5lib")
+    result_data = json.loads(r.content)
+    json_output = {}
+
+    # # Melting Point
+    # if 'Melting Point' in r.text:
+    json_output['Melting Point'] = []
+    json_output['Boiling Point'] = []
+    json_output['Freezing Point'] = []
+    json_output['Chemical/Compound'] = ''
+    json_output['Reaction Group ID'] = ''
+
+
+    # PubChem ID
+    json_output['PubChecm ID'] = result_data['Record']['RecordNumber']
+
+    sections = result_data['Record']['Section']
+    names_and_id = get_depth('Names and Identifiers', sections)
+    chem_phys_props = get_depth('Chemical and Physical Properties', sections)
+    sections = result_data['Record']['Section'][names_and_id]['Section']
+    record_title = get_depth('Record Title', sections)
+    computed_desc = get_depth('Computed Descriptors', sections)
+    other_id = get_depth('Other Identifiers', sections)
+    id_sections = result_data['Record']['Section'][names_and_id]['Section'][other_id]['Section']
+    synonyms = get_depth('Synonyms', sections)
+    syn_sections = result_data['Record']['Section'][names_and_id]['Section'][synonyms]['Section']
+
+    json_output['name'] = result_data['Record']['Section'][names_and_id]['Section'][record_title]['Information'][0]['StringValue']
+
+    computed_sections = result_data['Record']['Section'][names_and_id]['Section'][computed_desc]['Section']
+    if 'InChI Key' in r.text:
+        inchi_key_sec = get_depth('InChI Key', computed_sections)
+        json_output['InChI-key'] = result_data['Record']['Section'][names_and_id]['Section'][computed_desc]['Section'][inchi_key_sec]['Information'][0]['StringValue']
     else:
-        search_year = str(datetime.datetime.now().year)
-    term_info = {}
-    term_label = 'term'
-    start_label = 'start'
-    end_label = 'end'
-    print req_term + ' ' + search_year
-    r = requests.get(ucr_cal)
+        json_output['InChI-key'] = ''
+    if 'Canonical SMILES' in r.text:
+        c_smiles_sec = get_depth('Canonical SMILES', computed_sections)
+        json_output['Canonical SMILES'] = result_data['Record']['Section'][names_and_id]['Section'][computed_desc]['Section'][c_smiles_sec]['Information'][0]['StringValue']
+    else:
+        json_output['Canonical SMILES'] = ''
+    if 'Isomeric SMILES' in r.text:
+        i_smiles_sec = get_depth('Isomeric SMILES', computed_sections)
+        json_output['Isomeric SMILES'] = result_data['Record']['Section'][names_and_id]['Section'][computed_desc]['Section'][i_smiles_sec]['Information'][0]['StringValue']
+    else:
+        json_output['Isomeric SMILES'] = ''
+    if 'Molecular Formula' in r.text:
+        molecular_form = get_depth('Molecular Formula', sections)
+        json_output['molecular-formula'] = result_data['Record']['Section'][names_and_id]['Section'][molecular_form]['Information'][0]['StringValue']
+    else:
+        json_output['molecular-formula'] = ''
+    if 'CAS' in r.text:
+        cas_sec = get_depth('CAS', id_sections)
+        json_output['CAS-NUMBER'] = result_data['Record']['Section'][names_and_id]['Section'][other_id]['Section'][cas_sec]['Information'][0]['StringValue']
+    else:
+        json_output['CAS-NUMBER'] = ''
+    if 'UN Number' in r.text:
+        un_sec = get_depth('UN Number', id_sections)
+        json_output['un-number'] = result_data['Record']['Section'][names_and_id]['Section'][other_id]['Section'][un_sec]['Information'][0]['StringValue']
+    else:
+        json_output['UN Number'] = ''
+    if 'MeSH Synonyms' in r.text:
+        mesh_sec = get_depth('MeSH Synonyms', syn_sections)
+        json_output['MeSH synonyms'] = result_data['Record']['Section'][names_and_id]['Section'][synonyms]['Section'][mesh_sec]['Information'][0]['StringValueList']
+    else:
+        json_output['MeSH Synonyms'] = ''
+
+    sections = result_data['Record']['Section'][chem_phys_props]['Section']
+    computed_prop_dep = get_depth('Computed Properties', sections)
+    comp_prop_sec = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section']
+    exp_prop_dep = get_depth('Experimental Properties', sections)
+
+    if 'Molecular Weight' in r.text:
+        mol_weight = get_depth('Molecular Weight', comp_prop_sec)
+        json_output['molecular-weight'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][mol_weight]['Information'][0]['NumValue']
+    else:
+        json_output['Molecular Weight'] = ''
+    if 'Hydrogen Bond Donor Count' in r.text:
+        hy_bond = get_depth('Hydrogen Bond Donor Count', comp_prop_sec)
+        json_output['Hyrogen Bond Donor Count'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][hy_bond]['Information'][0]['NumValue']
+    else:
+        json_output['Hydrogen Bond Donor Count'] = ''
+    if 'Hydrogen Bond Acceptor Count' in r.text:
+        hy_bond_a = get_depth('Hydrogen Bond Acceptor Count', comp_prop_sec)
+        json_output['Hyrogen Bond Acceptor Count'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][hy_bond_a]['Information'][0]['NumValue']
+    else:
+        json_output['Hydrogen Bond Acceptor Count'] = ''
+    # if 'Rotatable Bond Count' in r.text:
+    #     json_output['Rotatable Bond Count'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][chem_phys_props]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Rotatable Bond Count'] = ''
+    # if 'Exact Mass' in r.text:
+    #     json_output['Exact Mass'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][5]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Exact Mass'] = ''
+    # if 'Heavy Atom Count' in r.text:
+    #     json_output['Heavy Atom Count'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][8]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Heavy Atom Count'] = ''
+    # if 'Formal Charge' in r.text:
+    #     json_output['Formal Charge'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][9]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Formal Charge'] = ''
+    # if 'Complexity' in r.text:
+    #     json_output['Complexity'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][10]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Complexity'] = ''
+    # if 'Covalently-Bonded Unit Count' in r.text:
+    #     json_output['Covalently-Bonded Unit Count'] = result_data['Record']['Section'][chem_phys_props]['Section'][computed_prop_dep]['Section'][16]['Information'][0]['NumValue']
+    # else:
+    #     json_output['Covalently-Bonded Unit Count'] = ''
+
+    # print json_output
+    # return r.json()
+    # return jsonify(**result_data)
+    return jsonify(**json_output)
+    # return soup.prettify()
+
+def get_depth(search_value, x):
+    temp = 0
+    level = 0
+    for section in x:
+        level = level + 1
+        for key, value in section.iteritems():
+            if value == search_value:
+                temp = level - 1
+                print search_value, temp
+                return temp
+
+def getPubChemID(compound, res_num):
+    url2get = base_ncbi + c_search_path + compound.lower()
+    r = requests.get(url2get, headers=headers)
     html = r.text
     soup = BeautifulSoup(html, "html.parser")
-    col_index = find_term_column(req_term,search_year,soup)
-    first_day = find_start_date(soup)
-    last_day = find_end_date(soup)
-    new_table = reconstruct_table(soup,first_day,last_day)
-    term_info[start_label] = convert_date(get_rdate(soup,col_index,1))
-    term_info[end_label] = convert_date(get_rdate(soup,col_index,2))
-    term_info[term_label] = req_term.upper()
-    term_info['year'] = search_year
-    return jsonify(**term_info)
-
+    cid = soup.findAll('dt', string='CID')
+    cidnum = []
+    for elem in soup(text=re.compile(r'CID:')):
+        cidnum.append(elem.parent.next_sibling.string)
+    print res_num
+    return cidnum[res_num]
 
 # Tabular test
 @app.route('/')
@@ -165,15 +294,6 @@ def find_end_date(soup):
                 else:
                     last_day = i
     return last_day
-
-
-def convert_date(date_str):
-    df = '%B %d, %Y'
-    do = '%Y-%m-%d'
-    if date_str > 1 and bool(re.compile('\d').search(date_str)):
-        return datetime.datetime.strptime(date_str, df).strftime(do)
-    else:
-        return -1
 
 
 def reconstruct_table(soup,first_day,last_day):
